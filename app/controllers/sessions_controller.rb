1e681edddd7550acabd521d500ca2e801b170950
SmithVyne <smithnkereuwem2@gmail.com>
2020-08-21 07:34:31 +0300

    add other controllers and sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+
+  def create
+    @user = User.find_by(name: params[:id])
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = ['You are sucessfully logged in']
+      redirect_to @user
+    else
+      flash[:errors] = ['Wrong login']
+      redirect_to login_path
+    end
   end
 end
