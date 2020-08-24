1060f6a4e3bce77b5fe7ce0d5683b0da1ca0e550
SmithVyne <smithnkereuwem2@gmail.com>
2020-08-24 07:19:11 +0300

    add login and registration pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :articles, foreign_key: 'author_id'
   has_many :votes
   
-  validates :name, presence: true,
+  validates :name, presence: true, uniqueness: true,
                    length: { maximum: 30 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
+
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
 end
