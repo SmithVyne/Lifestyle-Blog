1341a3b6866a9abdb39cec154b2d737fae02bbb7
SmithVyne <smithnkereuwem2@gmail.com>
2020-09-03 15:39:03 +0300

    Add strong parameters to votes_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,13 @@
 class VotesController < ApplicationController
   def create
-    @article = Article.find(params[:id])
+    @article = Article.find_by(vote_params)
     @vote = current_user.votes.new(article: @article)
     redirect_to articles_path if @vote.save
+  end
+  
+  private
+  
+  def vote_params
+    params.permit(:id)
   end
 end
